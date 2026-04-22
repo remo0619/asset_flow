6c5b564400fdaeff8b841a4ad2b82a7129b72971
remo0619 <ichinohe@aomori-innovators.com>
2026-04-22 11:46:23 +0900

    登録機器・未承認一覧は管理者のみに表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,17 @@ class UsersController < ApplicationController
   def show
     @devices = @user.devices
     if @user.admin?
-      @request_items = Request.where(approver_id: current_user.id, status: :pending)
+      # 未承認のみ抽出
+      @pending_requests = Request.where(approver_id: current_user.id, status: :pending)
+
+      # 承認済みのみ抽出
+      @requests_history = Request.where(approver_id: current_user.id).where.not(status: :pending)
+
+      # 自分の申請のみ抽出
+      @my_requests = Request.where(user_id: current_user.id)
+    else
+      # 自分の申請のみ抽出
+      @my_requests = Request.where(user_id: current_user.id)
     end
   end
 
